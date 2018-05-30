bba35e95200aed14ca820ad6e6c1bca64e7f4124
Peter Ren <lcdcdr2004@gmail.com>
2018-05-30 21:06:29 +0800

    Remove elixir runtime

--- runtimes/elixir/brotorift/lib/brotorift/brotorift.ex
+++
@@ -1,7 +0,0 @@
-defmodule Brotorift do
-  use Application
-
-  def start(_type, args) do
-    Brotorift.Supervisor.start_link(args)
-  end
-end
--- runtimes/elixir/brotorift/lib/brotorift/connection_behaviour.ex
+++
@@ -1,5 +0,0 @@
-defmodule Brotorift.ConnectionBehaviour do
-  @callback start_link({socket :: any(), transport :: module(), handler :: module()}) :: {:ok, pid()}
-  @callback stop(pid :: pid()) :: any()
-  @callback handle_data(pid :: pid(), data :: iodata()) :: any()
-end
